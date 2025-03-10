add8b8882b6fd3dd636dd340c29830aa816343f2
Nasif Shuvo <shuvo.nasif@gmail.com>
2025-03-10 12:26:36 +0600

    prod fix

--- src/components/client/Sidebar.tsx
+++ src/components/client/Sidebar.tsx
@@ -2,9 +2,12 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { HomeIcon, UserIcon, MegaphoneIcon, CreditCardIcon } from "@heroicons/react/24/outline";
-
+import { useEffect } from "react";
+import { useState } from "react";
+import { Client } from "@/types/client";
 export default function Sidebar() {
   const pathname = usePathname();
+  const [client, setClient] = useState<Client | null>(null);
 
   const navigation = [
     { name: 'Dashboard', href: '/dashboard/client', icon: HomeIcon },
@@ -20,13 +23,21 @@ export default function Sidebar() {
     // { name: 'Imports', href: '/dashboard/client/imports', icon: DocumentIcon },
     // { name: 'Settings', href: '/dashboard/admin/settings', icon: Cog6ToothIcon },
   ];
+  useEffect(() => {
+    const fetchClient = async () => {
+      const response = await fetch(`/api/clients/me`);
+      const data = await response.json();
+      setClient(data);
+    };
+    fetchClient();
+  }, []);
 
   return (
     <div className="hidden md:flex md:flex-shrink-0">
       <div className="flex flex-col w-64">
         <div className="flex flex-col h-0 flex-1">
           <div className="flex items-center h-16 flex-shrink-0 px-4 bg-gray-900">
-            <h1 className="text-xl font-bold text-white">Client Portal</h1>
+            <h1 className="text-xl font-bold text-white">{client?.name}&apos;s Portal</h1>
           </div>
           <div className="flex-1 flex flex-col overflow-y-auto bg-gray-800">
             <nav className="flex-1 px-2 py-4 space-y-1">
